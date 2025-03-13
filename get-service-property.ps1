ce4777d29da7ced04168da2a1ea15c5921755491
omerAlBa <45664296+omerAlBa@users.noreply.github.com>
2025-03-13 14:22:31 +0100

    [init] script to view all services and there properties

---
+++ get-service-property.ps1
@@ -0,0 +1,15 @@
+echo '[+] Service Enumaration'
+$services = Get-Service
+
+Foreach ( $service in $services ) {
+
+	$regPath = "HKLM:\System\CurrentControlSet\Services\$($service.name)"
+	if ( Test-Path $regPath ) {
+		echo ""
+		echo "###############"
+  		echo "Service name -> $($service.name)"
+   		$object =  Get-ItemProperty -Path $regpath
+		echo "---------------"
+		echo $object
+	}
+}
